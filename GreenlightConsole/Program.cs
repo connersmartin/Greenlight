efb2768283ca36cc6bb6e557df70862b0ccd55cd
Conner Martin <connersmartin@gmail.com>
2018-07-22 14:10:44 -0700

    Refactored to use colors and thinking about adding shows and members

--- GreenlightConsole/Program.cs
+++ GreenlightConsole/Program.cs
@@ -12,35 +12,21 @@ namespace GreenlightConsole
         const string line = "";
         public static int width = 2;
         public static int boxWidth = 15;
-        public static Show show = new Show(1, "Mannequins By Day", "test", new List<Member>());
-        public static string Red = "R";
-        public static string Yel = "Y";
-        public static string Gre = "G";
+        public static Show show; 
+        public static string Red = "N";
+        public static string Gre = "Y";
         
 
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-            //Manually added, but would use input eventually
-            Member Conner = new Member(1, "Conner", "Drums");
-            Member Vince = new Member(2, "Vince", "Guitar");
-            Member Evan = new Member(3, "Evan", "Bass");
-            Member Melinda = new Member(4, "Melinda", "Vox");
-            Member Megan = new Member(5, "Megan", "Vox");
-
-
-            show.Band.Add(Conner);
-            show.Band.Add(Vince);
-            show.Band.Add(Evan);
-            show.Band.Add(Melinda);
-            show.Band.Add(Megan);
+            show = GetShow();
+            GetMembers();
 
             width = show.Band.Count() * boxWidth + 2;
 
             bool start = false;
 
-            //constantly repeating until everyone's status is g
+            //constantly repeating until everyone's status is y
             while (!start)
             {
                 //clear the console and draw box
@@ -58,21 +44,19 @@ namespace GreenlightConsole
                 //capture current status
                 foreach (Member member in show.Band)
                 {
-                    if (member.Status != 2)
+                    if (member.Status != 1)
                     {
-                        Console.WriteLine(member.Name + "- R, Y, G: ");
+                        Console.Write(member.Name + " Ready? - y, n: ");
                         string stat = Console.ReadKey().KeyChar.ToString();
+                        Console.WriteLine();
 
                         switch(stat)
                         {
-                            case "r":
+                            case "n":
                                 member.Status = 0;
                                 break;
                             case "y":
                                 member.Status = 1;
-                                break;
-                            case "g":
-                                member.Status = 2;
                                 break;
                             default:
                                 break;
@@ -80,9 +64,12 @@ namespace GreenlightConsole
                     }
                 }
                 //exits the loop if everyone is ready
-                if (Ready(show))
+                if (Ready(show) == (ConsoleColor)10)
                 {
                     start = true;
+                    Console.Clear();
+                    Console.WriteLine(show.Name + ": Go Time!");
+                    BoxFill();
                 }
             }         
             Console.ReadLine();
@@ -102,9 +89,12 @@ namespace GreenlightConsole
         //fills the box with stars
         public static void BoxFill()
         {
-            Console.WriteLine(line.PadRight(width, '*'));
-            Console.WriteLine(StatusFill() + " *");
-            Console.WriteLine(line.PadRight(width, '*'));
+            //need to refactor for red/yellow/green box 12,14,10 in ConsoleColor
+            Console.BackgroundColor = Ready(show);
+            Console.WriteLine(line.PadRight(width, ' '));
+            Console.WriteLine(StatusFill() + "  ");
+            Console.WriteLine(line.PadRight(width, ' '));
+            Console.BackgroundColor = 0;
         }
 
         //fills band member status
@@ -112,38 +102,72 @@ namespace GreenlightConsole
         {
             foreach (Member member in show.Band)
             {
+                Console.BackgroundColor = 0;
                 switch(member.Status)
                 {
+                    
                     case 0:
                         Console.Write(Red.PadRight(boxWidth));
                         break;
                     case 1:
-                        Console.Write(Yel.PadRight(boxWidth));
-                        break;
-                    case 2:
                         Console.Write(Gre.PadRight(boxWidth));
                         break;
                     default:
                         break;
                 }
-            }
+            Console.BackgroundColor = Ready(show);
+        }
             return null;
         }
 
         //checks to see if everyone has a green status, need a better way for a global status check... if all are red, red, if at least one is green, yellow
-        public static bool Ready(Show show)
+        public static ConsoleColor Ready(Show show)
         {
             var check = 0;
 
             foreach (Member member in show.Band)
             {
-                if (member.Status == 2)
+                if (member.Status == 1)
                 {
                     check++;
                 }
             }
-            return check == show.Band.Count();
+
+            if (check == show.Band.Count())
+            {
+                return (ConsoleColor)10;
+            }
+            else if (check < show.Band.Count() && check > 0)
+            {
+                return (ConsoleColor)14;
+            }
+            else
+            {
+                return (ConsoleColor)12;
+            }
         }
 
+        private static Show GetShow()
+        {
+            return new Show(1, "Mannequins By Day", "test", new List<Member>());
+        }
+
+        private static void GetMembers()
+        {
+            //would want to have option for using name or instrument depending on familiarity
+
+            //Manually added, but would use input eventually
+            Member Conner = new Member(1, "Conner", "Drums");
+            Member Vince = new Member(2, "Vince", "Guitar");
+            Member Evan = new Member(3, "Evan", "Bass");
+            Member Melinda = new Member(4, "Melinda", "Vox");
+            Member Megan = new Member(5, "Megan", "Vox");
+
+            show.Band.Add(Conner);
+            show.Band.Add(Vince);
+            show.Band.Add(Evan);
+            show.Band.Add(Melinda);
+            show.Band.Add(Megan);
+        }
     }
 }
